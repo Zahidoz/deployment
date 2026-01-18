6550328d9b11506a051307073baaeaf10142d3cf
Zahid <zahid.vahabzade@gmail.com>
2026-01-18 19:01:51 +0400

    added users route

--- index.js
+++ index.js
@@ -12,9 +12,20 @@ const products = [
   { id: 2, name: 'Product B', price: 150 },
   { id: 3, name: 'Product C', price: 200 },
 ];
+// example get method users
+const users = [
+  { id: 1, name: 'User A', email: 'usera@example.com' },
+  { id: 2, name: 'User B', email: 'userb@example.com' },
+  { id: 3, name: 'User C', email: 'userc@example.com' },
+];
+
 
 app.get('/products', (req, res) => {
   res.status(200).json(products);
+});
+
+app.get('/users', (req, res) => {
+  res.status(200).json(users);
 });
 
 app.listen(PORT, () => {
